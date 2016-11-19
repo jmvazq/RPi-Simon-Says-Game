a6ea75aa4305516fece785d525a0bd5053b118fa
Arwym <contact@jmvazquez.com>
2016-11-19 19:41:51 +0000

    Defined initial inputs, default colors, start function, play function. Imported requests library. Next: start coding the game mechanics, high scores and name input; finally: posting high score to gamescores server.

--- simon.py
+++ simon.py
@@ -1,37 +1,67 @@
 #!/usr/bin/python
-# The humble beginnings of my Pymon Says game...
+# The humble beginnings of my Simon Says game...
 import math
 import time
 
 import Adafruit_CharLCD as LCD
 
+import requests
+
 
 # Initialize the LCD using the pins 
 lcd = LCD.Adafruit_CharLCDPlate()
 
-# Show a welcome message
-lcd.message('Sup? I'm Pymon.')
-time.sleep(3.0)
-lcd.clear()
-lcd.message('Let's start playing. Do your best! ;)')
+# Default colors
+WHITE = (1, 1, 1)
+RED =  (1, 0, 0)
+GREEN = (0, 1, 0)
+BLUE = (0, 0, 1)
+YELLOW = (1, 1, 0)
 
-# Show button state.
-lcd.clear()
-lcd.message('Press buttons...')
+# Show startup message
+def start():
+	# Reset light
+	lcd.set_color(WHITE[0], WHITE[1], WHITE[2])
 
-# Make list of button value, text, and LED color.
-buttons = ( (LCD.SELECT, 'Select', (1,1,1)),
-            (LCD.LEFT,   'Left/Red'  , (1,0,0)),
-            (LCD.UP,     'Up/Blue'    , (0,0,1)),
-            (LCD.DOWN,   'Down/Green'  , (0,1,0)),
-            (LCD.RIGHT,  'Right/Yellow' , (1,1,0)) )
+	# Show a welcome message
+	lcd.message('Sup?! I\'m Pymon.')
+	time.sleep(3.0)
+	lcd.clear()
+	lcd.message('Let\'s start \nplaying.')
 
-print 'Press Ctrl-C to quit.'
-while True:
-	# Loop through each button and check if it is pressed.
-	for button in buttons:
-		if lcd.is_pressed(button[0]):
-			# Button is pressed, change the message and backlight.
+	play()
+
+# Start game
+def play():
+	print 'Press Ctrl-C to quit.'
+	
+	# Show button state.
+	time.sleep(3.0)
+	lcd.clear()
+	lcd.message('Press SELECT \nto start.')
+
+	# Encouragement!
+	while True:
+		if lcd.is_pressed(LCD.SELECT):
+			time.sleep(1.0)
 			lcd.clear()
-			lcd.message(button[1])
-			lcd.set_color(button[2][0], button[2][1], button[2][2])
+			lcd.message('Do your best! ;)')
+
+			# Make list of button value, text, and LED color.
+			buttons = ( (LCD.SELECT, 'Select', (1,1,1)),
+        	    		(LCD.LEFT,   'Left/Red'  , (1,0,0)),
+        	    		(LCD.UP,     'Up/Blue'    , (0,0,1)),
+        	    		(LCD.DOWN,   'Down/Green'  , (0,1,0)),
+        	    		(LCD.RIGHT,  'Right/Yellow' , (1,1,0)) )
+
+			while True:
+				# Loop through each button and check if it is pressed.
+				for button in buttons:
+					if lcd.is_pressed(button[0]):
+						# Button is pressed, change the message and backlight.
+						lcd.clear()
+						lcd.message(button[1])
+						lcd.set_color(button[2][0], button[2][1], button[2][2])
+
+# Run code
+start()
